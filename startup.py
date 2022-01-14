1a8ff92579eb038a397a38c46ba4d1a1e1f41c7e
Mac0r0ni_And_Weed <61136278+Mac0r0ni@users.noreply.github.com>
2022-01-14 03:07:08 -0500

    Add files via upload

---
+++ startup.py
@@ -0,0 +1,17 @@
+'''
+This is where you add details to speed up the bootup process.
+'''
+use_preset = False
+
+preset_username = ""
+
+preset_password = ""
+
+preset_num_of_bots = 0 
+
+'''
+This is where you set the message to send to users
+'''
+preset_message = "Join the realist shit on kik kik.me/g/slqY7aAUVLCvFy3lDZY9mqo1HTo"
+
+ 
